33526beae429c9f4e9f5781a246e4a7dba34f725
Egemen Tutmuş <egementutmus@Egemen-Mac-mini.local>
2026-06-22 14:38:48 +0300

    fix: remove nginx www redirect, revert to clean config

--- nginx-entrypoint.sh
+++ nginx-entrypoint.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 PORT=80
 [ -n "$PORT" ] && PORT="$PORT"
-cat > /etc/nginx/conf.d/default.conf << NGINXEOF
-server {
-    listen $PORT;
-    server_name www.gizemegemenwedding.online;
-    return 301 https://gizemegemenwedding.online\$request_uri;
-}
-
+cat > /etc/nginx/conf.d/default.conf << EOF
 server {
     listen $PORT;
     root /usr/share/nginx/html;
     index izmir.html;
     try_files \$uri \$uri.html \$uri/ =404;
 }
-NGINXEOF
+EOF
 exec "$@"
